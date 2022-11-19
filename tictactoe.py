eabd7c34133faff774822efc3e0b8a29a5fd9955
DevPapel <cool.jef77@gmail.com>
2022-11-19 21:32:05 +0800

    Implementation of diagonal condition to check winner if gameboard[0-4-8] is filled with same symbol

--- tictactoe.py
+++ tictactoe.py
@@ -82,7 +82,14 @@ while True:
     if "☐" not in gameboard:
         print("Game Ended with a tie!")
         exit()
-
+    diagonal1 = gameboard[0] == gameboard[4] == gameboard[8] != "☐"
+    if diagonal1:
+        winner = gameboard[0]
+        if winner == "X":
+            print(get_player1 + " won.")
+        elif winner == "O":
+            print(get_player2 + " won.")
+        exit()
 
     
 
