e835188afce2016490ca1338bb71672bac587811
DevPapel <cool.jef77@gmail.com>
2022-11-19 20:03:11 +0800

    Implementation of Game Board for the Tic Tac Toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,6 @@
 # This program allows you to play Tic Tac Toe with your friend!
 # Program created by: Jeferson A. Tadios
+
+gameboard = ["☐", "☐", "☐",
+         "☐", "☐", "☐",
+         "☐", "☐", "☐"]
